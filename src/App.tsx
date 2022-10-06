1604adfa5360046bc001ab00b6e84094d3799b58
sharglut <p.filimon75@gmail.com>
2022-10-06 22:58:56 +0200

    setup du rendu jsx et debut de definition des fonctions

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./style/App.scss";
+
+interface LengthWeakness {
+  message?: string;
+  deduction?: number;
+}
 
 function App() {
+  const [weakness, setWeakness] = useState<LengthWeakness>({});
+  const [weaknesses, setWeaknesses] = useState<LengthWeakness[]>([]);
+
+  const calculatePasswordStrength = (password: string): LengthWeakness[] => {
+    setWeaknesses([...weaknesses, lengthWeakness(password)]);
+    return weaknesses;
+  };
+
+  const lengthWeakness = (password: string): LengthWeakness => {
+    const length: number = password.length;
+    if (length < 5) {
+      setWeakness({ message: "Your password is too short", deduction: 40 });
+      return weakness;
+    }
+
+    if (length < 10) {
+      setWeakness({ message: "Your password could be longer", deduction: 15 });
+      return weakness;
+    } else {
+      setWeakness({ message: "Your password is solid", deduction: 0 });
+      return weakness;
+    }
+  };
+
+  // const updateStrengthMeter = () => {};
+
+  const handleInput = (e: any) => {
+    calculatePasswordStrength(e.target.value);
+    console.log(weaknesses);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="header">Password Strength Test</h1>
+      <div className="strengh-meter"></div>
+      <input
+        type="text"
+        id="input-password"
+        className="input-password"
+        autoFocus
+        aria-labelledby="password"
+        placeholder="password"
+        onInput={handleInput}
+      />
+      <div id="reasons" className="reasons"></div>
     </div>
   );
 }
