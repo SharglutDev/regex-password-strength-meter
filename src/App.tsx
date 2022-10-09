3c50cdd66574f58f73fe13858c1120ed92217c36
sharglut <p.filimon75@gmail.com>
2022-10-09 19:17:19 +0200

    ajout strength meter + characters condition

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,92 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./style/App.scss";
 
 interface LengthWeakness {
-  message?: string;
-  deduction?: number;
+  message: string;
+  deduction: number;
 }
 
 function App() {
-  // const [weakness, setWeakness] = useState<LengthWeakness>({});
-  const [weaknesses, setWeaknesses] = useState<LengthWeakness[]>([]); // obligé de push la première valeur de weakness ?
+  const [inputPassword, setInputPassword] = useState<string>("");
+  const [weaknesses, setWeaknesses] = useState<LengthWeakness[]>([
+    { message: "Enter your password", deduction: 100 },
+  ]);
+
+  const lengthWeakness = (password: string) => {
+    const length: number = password.length;
+    if (length === 0) {
+      setWeaknesses([{ message: "Enter your password", deduction: 100 }]);
+    } else if (length < 5) {
+      setWeaknesses([{ message: "Your password is too short", deduction: 40 }]);
+    } else if (length < 10) {
+      setWeaknesses([
+        { message: "Your password could be longer", deduction: 15 },
+      ]);
+    } else {
+      setWeaknesses([
+        { message: "Your password length is good", deduction: 0 },
+      ]);
+    }
+  };
+
+  const characterTypeWeakness = (
+    password: string,
+    regex: RegExp,
+    type: string
+  ) => {
+    const test = regex.test(password);
+    if (!test) {
+      setWeaknesses((weaknesses) => [
+        ...weaknesses,
+        {
+          message: `Your password need at least one ${type}`,
+          deduction: 10,
+        },
+      ]);
+    }
+  };
+
+  const repeatedCharacterWeakness = (password: string) => {
+    const regex = /(.)\1/;
+    const test = regex.test(password);
+    if (test) {
+      setWeaknesses((weaknesses) => [
+        ...weaknesses,
+        {
+          message: `Your password has repeated characters`,
+          deduction: 20,
+        },
+      ]);
+    }
+  };
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const passwordValue = e.target.value;
+    setInputPassword(passwordValue);
+  };
 
   useEffect(() => {
-    console.log(weaknesses);
-  }, [weaknesses]);
-
-  const calculatePasswordStrength = (password: string): LengthWeakness[] => {
-    setWeaknesses([...weaknesses, lengthWeakness(password)]);
-    return weaknesses;
-  };
-
-  const lengthWeakness = (password: string): LengthWeakness => {
-    const length: number = password.length;
-    if (length < 5) {
-      let weakness = { message: "Your password is too short", deduction: 40 };
-      return weakness;
-    }
-
-    if (length < 10) {
-      let weakness = {
-        message: "Your password could be longer",
-        deduction: 15,
-      };
-      return weakness;
-    } else {
-      let weakness = { message: "Your password is solid", deduction: 0 };
-      return weakness;
-    }
-  };
-
-  // const updateStrengthMeter = () => {};
-
-  const handleInput = (e: any) => {
-    calculatePasswordStrength(e.target.value);
-  };
+    lengthWeakness(inputPassword);
+    characterTypeWeakness(inputPassword, /[a-z]/, "lowercase");
+    characterTypeWeakness(inputPassword, /[A-Z]/, "uppercase");
+    characterTypeWeakness(inputPassword, /\d/, "number");
+    characterTypeWeakness(inputPassword, /[^\w\s]/, "special character");
+    repeatedCharacterWeakness(inputPassword);
+  }, [inputPassword]);
 
   return (
     <div className="container">
       <h1 className="header">Password Strength Test</h1>
-      <div className="strengh-meter"></div>
+      <div className="strengh-meter">
+        <div
+          style={{
+            width: `${
+              100 - weaknesses.reduce((prev, curr) => prev + curr.deduction, 0)
+            }%`,
+          }}
+          className="strength-meter-bar"
+        ></div>
+      </div>
       <input
         type="text"
         id="input-password"
@@ -57,7 +96,14 @@ function App() {
         placeholder="password"
         onChange={handleInput}
       />
-      <div id="reasons" className="reasons"></div>
+      <div id="reasons" className="reasons">
+        {weaknesses.map((weakness, index) => (
+          <div key={`weakness ${index}`}>{weakness.message}</div>
+        ))}
+        <div style={{ fontWeight: "bold" }}>
+          {weaknesses.length === 1 && "Your password is strong"}
+        </div>
+      </div>
     </div>
   );
 }
